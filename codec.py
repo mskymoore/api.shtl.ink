10ed97b86a7f18515ce4ae55bcc8646dc71793fb
sky moore <mskymoore@gmail.com>
2022-06-10 00:55:28 -0500

    more efficient and clean, 3000 test urls

--- codec.py
+++ codec.py
@@ -3,7 +3,8 @@
 
 import pytest
 import random
-from sqlalchemy import Column, Integer, String, create_engine, delete
+from sqlalchemy import Column, String, create_engine, delete
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session, declarative_base
 
 
@@ -12,16 +13,15 @@ db = create_engine('sqlite:///url_records.db', echo=False, future=True)
 Base = declarative_base()
 
 
-class URLTable(Base):
+class ShortURLModel(Base):
     """
-    URLTable: Schema for database table with three columns, id(the primary key),
+    ShortURLModel: Schema for database table with three columns, id(the primary key),
         long_url(the original url), and short_url(the shortened url)
     """
     __tablename__ = 'short_urls'
-    id = Column(Integer, primary_key=True)
     # 2000 characters is defacto max url length
-    long_url = Column(String(2000))
-    short_url = Column(String(2000))
+    long_url = Column(String(2000), unique=True)
+    short_url = Column(String(2000), primary_key=True)
 
     def __repr__(self):
         return f"URL(id={self.id!r}, url={self.long_url!r}, \
@@ -39,8 +39,10 @@ def db_session(function):
         supplies it to the passed function object with a context manager if it's absent
     """
     def get_session(*args, **kwargs):
+
         if 'session' in kwargs and isinstance(kwargs['session'], Session):
             short_url = function(*args, **kwargs)
+
         else:
             with Session(db) as session:
                 short_url = function(session=session, *args, **kwargs)
@@ -61,6 +63,7 @@ class Codec:
     """
 
     def __init__(self):
+        # number of available urls in this config is 13,841,287,201
         # no vowels, no visually ambiguous characters
         self.alphabet = '23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
         self.base = len(self.alphabet) - 1
@@ -68,64 +71,62 @@ class Codec:
         self.shift_range = range(2, 8)
         self.shift_bits = 12
 
-    def primary_key_encode(self, primary_key: int, session: Session, multiplier=333) -> str:
+    def url_encode(self, long_url: str, session: Session, multiplier=333) -> str:
         shift_bits = self.shift_bits
         short_url = ''
-        new_key = primary_key * multiplier
+        seed = random.randint(1000, 13841287201)
+        new_key = seed * multiplier
 
         for _ in self.shift_range:
             short_url += self.alphabet[(new_key >> shift_bits) & self.base]
             shift_bits -= 2
 
-        url_records = session.query(URLTable)\
-            .filter(URLTable.short_url == short_url).all()
+        try:
+            session.add(ShortURLModel(long_url=long_url, short_url=short_url))
+            session.commit()
+            return short_url
 
-        if len(url_records) != 0:
-            # collision
-            short_url = self.primary_key_encode(primary_key, session,
-                                                multiplier=multiplier +
-                                                random.choice(self.shift_range))
-        return short_url
+        # short_url collision
+        except IntegrityError as e:
+            session.rollback()
+            session.execute(delete(ShortURLModel).where(ShortURLModel.long_url == long_url))
+            short_url = self.url_encode(long_url, session,
+                                            multiplier=multiplier +
+                                            random.choice(self.shift_range))
+            return short_url
+            # although this should work, it doesn't with sqllite
+            # must delete and re-add instead, otherwise eventually a wrong url
+            # is returned on decode
+            # try:
+            #     session.execute(update(ShortURLModel).where(
+            #         ShortURLModel.long_url == long_url).values(short_url=short_url))
+            #     session.commit()
+            #     return short_url
+
+            # except IntegrityError as e:
+            #     session.rollback()
+            #     short_url = self.url_encode(long_url, session,
+            #                                 multiplier=multiplier +
+            #                                 random.choice(self.shift_range))
+            #     return short_url
 
     @db_session
     def encode(self, long_url: str, session=None) -> str:
-        
+
         if len(long_url) > 2000:
             raise Exception("URL is too long, max length is 2000 characters.")
 
-        # check for url in db
-        url_record = session.query(URLTable)\
-            .filter(URLTable.long_url == long_url).first()
-
-        # add if not present
-        if url_record is None:
-            session.add(URLTable(long_url=long_url, short_url=""))
-            url_record = session.query(URLTable)\
-                .filter(URLTable.long_url == long_url).first()
-            short_url = self.primary_key_encode(url_record.id, session)
-            setattr(url_record, 'short_url', short_url)
-            return short_url
-
-        # overwrite if present
-        else:
-            session.execute(delete(URLTable)
-                            .where(URLTable.id == url_record.id))
-            short_url = self.encode(long_url, session=session)
-
-        return short_url
+        return self.url_encode(long_url, session)
 
     @db_session
     def decode(self, short_url: str, session=None) -> str:
-        url_records = session.query(URLTable)\
-            .filter(URLTable.short_url == short_url).all()
+        url_record = session.get(ShortURLModel, (short_url))
 
-        num_records = len(url_records)
-        if num_records > 1:
-            raise Exception("More than one record returned.")
-        elif num_records == 0:
-            return None
+        if url_record is not None:
+            return url_record.long_url
         else:
-            return url_records[0].long_url
+            return None
+
 
 
 if __name__ == "__main__":
@@ -136,9 +137,13 @@ if __name__ == "__main__":
         urls = file.read().splitlines()
 
     for i, url in enumerate(urls, 1):
+
+        if i == 3000:
+            break
         url = url.strip()
         encoded = codec.encode(url)
         decoded = codec.decode(encoded)
+
         if decoded != url:
             raise Exception("Returned the wrong url.")
         print(
